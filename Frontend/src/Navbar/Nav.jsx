38c87e54a484646a07f08ad44337e1ffec900938
nandiayan73 <nandiayan73@gmail.com>
2024-07-24 10:25:06 +0530

    changes

--- Frontend/src/Navbar/Nav.jsx
+++ Frontend/src/Navbar/Nav.jsx
@@ -1,21 +1,77 @@
 import { useMediaQuery } from "@chakra-ui/react";
+import {
+	Drawer,
+	DrawerBody,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerOverlay,
+	DrawerContent,
+	DrawerCloseButton,
+	Button,
+	useDisclosure,
+	Stack,
+	Box,
+  } from '@chakra-ui/react'
+  import React from "react";
 const Nav=()=>{
+	const { isOpen, onOpen, onClose } = useDisclosure()
+	const btnRef = React.useRef()
 	const [isMobile] = useMediaQuery("(max-width: 1000px)");
     return (
 	<>
 
-	<header style={{display:isMobile?"none":""}} className="header2">
+	<header  className="header2">
 		<div className="container2">
-	<h1 className="logo2" style={{fontFamily:"Playfair Display:", fontWeight:"bolder"}}>AN</h1>
+	<h1 className="logo2" style={{fontFamily:"Playfair Display:", fontWeight:"bolder", color:"rgba(128, 0, 128, 0.8)"}}>Ayan Nandi</h1>
 			{/* <a class="navbar-brand logo_h" href="index.html"><img style={{width:"25%",border:"0px",margin:"0px"}} src="photos/logo.png" alt=""></img></a> */}
 
 			<nav>
-				<ul className="nav-links2">
-					<li><a href="#">HOME</a></li>
-					<li><a href="#">ABOUT</a></li>
-					<li><a href="#">HOBBIES</a></li>
-					<li><a href="#">POSTS</a></li>
-					<li><a href="#">CONTACT</a></li>
+			<Button display={isMobile?"":"none"}   color={"white"} backgroundColor={" #800080"}  padding={"0px"} onClick={onOpen}>
+			≡
+			</Button>
+			<Drawer 
+				display={isMobile?"":"none"}
+				isOpen={isOpen}
+				placement='right'
+				// initialFocusRef={firstField}
+				onClose={onClose}
+			>
+				<DrawerOverlay />
+				<DrawerContent>
+				<DrawerCloseButton />
+				<DrawerHeader borderBottomWidth='1px' color={" rgba(128, 0, 128, 1)"}>
+					<img src="/photos/me.jpg" style={{width:"10%",display:"inline"}}></img>
+					Explore
+				</DrawerHeader>
+
+				<DrawerBody>
+					<Stack spacing='24px'>
+					<Box>
+						<ul   style={{listStyle:"none"}} className="drawNav">
+							<li style={{marginBottom:"1%"}}><a href="/" >HOME</a></li>
+							<li style={{marginBottom:"1%"}}><a href="/about" >ABOUT</a></li>
+							<li style={{marginBottom:"1%"}}><a href="/hobbies" >HOBBIES</a></li>
+							<li style={{marginBottom:"1%"}}><a href="/posts" >POSTS</a></li>
+							<li style={{marginBottom:"1%"}}><a href="/contact" >CONTACT</a></li>
+						</ul>
+					</Box>
+					</Stack>
+				</DrawerBody>
+
+				<DrawerFooter borderTopWidth='1px'>
+					<Button variant='outline' mr={3} onClick={onClose}>
+					Cancel
+					</Button>
+					{/* <Button colorScheme='blue'>Submit</Button> */}
+				</DrawerFooter>
+				</DrawerContent>
+			</Drawer>
+				<ul  style={{display:isMobile?"none":""}} className="nav-links2">
+					<li><a href="/">HOME</a></li>
+					<li><a href="/about">ABOUT</a></li>
+					<li><a href="hobbies">HOBBIES</a></li>
+					<li><a href="/posts">POSTS</a></li>
+					<li><a href="contact">CONTACT</a></li>
 				</ul>
 			</nav>
 		</div>
--- Frontend/src/front pages/Intro.jsx
+++ Frontend/src/front pages/Intro.jsx
@@ -1,6 +1,71 @@
-import { useMediaQuery } from "@chakra-ui/react";
+import { useMediaQuery,Button } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import "./intro.css"
 const Intro=()=>{
+	// Hooks:
 	const [isMobile] = useMediaQuery("(max-width: 1000px)");
+	const [roles,setRoles]=useState(["Developer","Programmer","AI/ML Enthusiast"]);
+	const [role,setRole]=useState("");
+	const [rolesDisplay,setRolesDisplay]=useState("");
+	const [coder,setCoder]=useState("none");
+	const [target,setTarget]=useState("");
+	const [trans,setTrans]=useState("none");
+	
+
+	useEffect(()=>{
+		{changeRoles()}
+	},[])
+	// functions:
+	const changeRoles=()=>{
+		let i=0;
+		let right= true;
+		let r=0;
+		
+		setInterval(()=>{
+			setRole(roles[r].slice(0,i));
+			if(right)
+				i++;
+			else
+				i--;
+			if(i>roles[r].length)
+				right=false;
+			if(i==0)
+			{
+				r=(r+1)%roles[r].length;
+				right=true;
+				i=1;
+			}
+			if(r==roles.length)
+			{
+				setRolesDisplay(" ");
+				setCoder("");
+				{changeTarget()}
+
+			}
+		}, 150);
+	}
+	
+	const changeTarget=()=>
+	{
+		let i=0;
+		const word="CODER";
+		setInterval(()=>{
+			setTarget(word.slice(0,i));
+			i++;
+			if(i>word.length)
+				setTrans("transform 2s ease-in-out");
+
+				
+		}, 300);
+	}
+
+
+	const openPdf=()=>{
+		const pdf="/documents/resume.pdf";
+		window.open(pdf, '_blank');
+	}
+	
+
     return(
     <>
        {/* <!--================ Start Home Banner Area =================--> */}
@@ -8,11 +73,12 @@ const Intro=()=>{
 	<section  style={{marginTop:isMobile?"0%":"10%"}}className="home_banner_area">
 		<div className="banner_inner">
 		<center>
-		<img style={{marginBottom:"0px",width:"40%",borderRadius:"200px",display:isMobile?"":"none",margin:"0px",border:"0px"}} src="photos/img2.gif" alt="cartoon"></img>
+		{/* <img style={{marginBottom:"0px",objectPosition:"50% 20%",width:"40%",borderRadius:"200px",display:isMobile?"":"none",margin:"0px",border:"0px",position:"relative",bottom:"40px",transition: "transform 2s ease-in-out",transform:"rotate(360deg)"}} src="photos/me2.png" alt="cartoon"></img> */}
+		<img style={{marginBottom:"0px",width:"60%",borderRadius:"200px",display:isMobile?"":"none",margin:"0px",border:"0px",position:"relative",bottom:"130px"}} src="photos/img2.gif" alt="cartoon"></img>
 		</center>
-			<div className="container">
+			<div className="container" style={{position:isMobile?"relative":"",bottom:"190px"}}>
 					
-				<div className="row">
+				<div className="row" >
 					<div className="col-lg-7">
 						<div className="banner_content">
 						{/* watermarks	 */}
@@ -23,23 +89,34 @@ const Intro=()=>{
 								.
 							</div> */}
 						
-							<div className="watermark" style={{left: "50%",top:"10%",borderRadius:"100%",backgroundColor:" rgba(128, 0, 128, 0.5)",opacity:"0.5",width:"50px",height:"10%"}}>
+							<div className="watermark" style={{position:isMobile?"relative":"fixed",bottom:"200px",left: "50%",top:"10%",borderRadius:"100%",backgroundColor:" rgba(128, 0, 128, 0.5)",opacity:"0.5",width:"50px",height:"10%"}}>
 								.
 							</div>
-							<div className="watermark" style={{top:"200px",left:"300px",right:"20px",bottom:"200px",fontSize:"50px"}}>
+							<div className="watermark" style={{top:"200px",left:"300px",right:"200px",bottom:"200px",fontSize:"50px"}}>
 							💡
 							</div>
 
 							<h3 className="text-uppercase">Hell0</h3>
 							<h1 className="text-uppercase">I am Ayan Nandi</h1>
-							<h5 className="text-uppercase">Computer Science Student</h5>
+							<h5 style={{color:"rgba(128, 0, 128, 0.8)",padding:"0px",margin:"0px",marginBottom:"10px",overflow:"hidden",display:rolesDisplay}}>{role}  <h5 style={{color:"rgba(128, 0, 128, 0.8)",transition: {trans},transform:"rotate(360)",display:"inline"}} >{target}</h5>
+							</h5>
+							
+							<h5 className="text-uppercase" >Computer Science Student</h5>
 							<div className="d-flex align-items-center">
-								<a className="primary_btn" href="/about"><span>Know More</span></a>
-								<a className="primary_btn tr-bg" href="#"><span>Get CV</span></a>
+								<a className="primary_btn" href="/about" style={{display:isMobile?"none":""}} ><span>Know More</span></a>
+								<a className="primary_btn tr-bg" href="#" style={{display:isMobile?"none":""}} onClick={openPdf}><span>Get CV</span></a>
+								{/*  For Mobile  */}
+								<Button display={isMobile?"":"none"} backgroundColor={"white"}  border={"2px solid rgba(128, 0, 128, 0.8)"}> 
+									<a href="/about"> Know More</a>
+								</Button>
+								<Button marginLeft={"10px"} style={{display:isMobile?"":"none"}} color={"white"} backgroundColor="rgba(128, 0, 128, 0.8)" onClick={openPdf}>
+									Get CV
+								</Button>
+
 							</div>
 						</div>
 					</div>
-				<img style={{display:"inline",width:"40%",borderRadius:"200px",display:isMobile?"none":""}} src="photos/img2.gif" alt="cartoon"></img>
+				<img style={{width:"40%",borderRadius:"200px",display:isMobile?"none":""}} src="photos/img2.gif" alt="cartoon"></img>
 
 					{/* <div className="col-lg-5">
 						<div className="home_right_img">
